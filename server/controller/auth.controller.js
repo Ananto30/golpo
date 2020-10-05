1b5fa2b6f2413008f012e8c8695a1783e5b3f6ff
Ananto30 <azizulhaq.ananto@gmail.com>
2020-10-06 02:37:57 +0600

    Add google apis in server and implement oauth login in backend

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const { body, validationResult } = require("express-validator");
 
 const authService = require("../service/auth.service");
+const googleAuthService = require("../service/google.auth.service");
 
 exports.login = async (req, res) => {
   try {
@@ -13,7 +14,10 @@ exports.login = async (req, res) => {
 
     const { username, password } = req.body;
 
-    const token = await authService.verifyUserAndGenerateToken(username, password);
+    const token = await authService.verifyUserAndGenerateToken(
+      username,
+      password
+    );
 
     res.status(200).json({ access_token: token });
   } catch (err) {
@@ -26,7 +30,41 @@ exports.googleLogin = async (req, res) => {
   try {
     const { data } = req.body;
 
-    token = await authService.findOrCreateGoogleUserAndGenerateToken(data);
+    const token = await authService.findOrCreateGoogleUserAndGenerateToken(
+      data
+    );
+
+    res.status(200).json({ access_token: token });
+  } catch (err) {
+    res.status(500).json({ errors: err.message });
+    return;
+  }
+};
+
+exports.getGoogleAuthUrl = async (req, res) => {
+  try {
+    const url = await googleAuthService.getGoogleAuthUrl();
+
+    res.status(200).json({ auth_url: url });
+  } catch (err) {
+    res.status(500).json({ errors: err.message });
+    return;
+  }
+};
+
+exports.getTokenByGoogleCode = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+      return;
+    }
+
+    const { code } = req.body;
+
+    const token = await googleAuthService.getTokenByGoogleCode(code);
+    console.log(token);
 
     res.status(200).json({ access_token: token });
   } catch (err) {
@@ -42,6 +80,9 @@ exports.validate = (method) => {
         body("username", "username is required").exists(),
         body("password", "password is required").exists(),
       ];
+    }
+    case "getTokenByGoogleCode": {
+      return [body("code", "code is required").exists()];
     }
   }
 };
